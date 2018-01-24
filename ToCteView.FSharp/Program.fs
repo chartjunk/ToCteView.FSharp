3ed7ac865cc4c7c63a9c3644dfacc16ed9516d68
Joona Luoma <joona.luoma@solteq.com>
2018-01-24 23:14:26 +0200

    Add project files.

---
+++ ToCteView.FSharp/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+open System.Windows.Forms
+
+let newline = Environment.NewLine
+let tab = "\t"
+
+let split (splitter:string) (value:string) = value.Split([|splitter|], StringSplitOptions.None) |> List.ofArray
+let splitRow = 
+    split tab 
+    >> List.map (function
+    | x when x = "NULL" -> x
+    | x -> sprintf "'%s'" (x.Replace("'", "''")))
+
+let rec rowsToSelects = 
+    function
+    | x::_, _ when x = "" -> ""
+    | x::xs, h -> (List.fold2 (sprintf "%s%s AS [%s], ") (newline + "UNION ALL SELECT ") (splitRow x) h).TrimEnd(' ', ',') + rowsToSelects (xs,h)
+    | _ -> ""
+
+let toCteView = 
+    split newline 
+    >> function
+    | h::cs -> [";WITH mycte AS ( "] @ [rowsToSelects(cs, split tab h).[12..]] @ [") SELECT * FROM mycte"]
+    | _ -> []
+    >> String.concat newline
+
+[<EntryPoint; STAThread>]
+let main argv =
+    Clipboard.GetText() |> toCteView |> Clipboard.SetText
+    0
